c80145752b11e12f7f005e92e2c6a0c34ccbce51
Rishu764 <rishabhbhatt218@gmail.com>
2025-12-30 08:05:42 +0530

    learn/ file system stream section

--- streams/index.js
+++ streams/index.js
@@ -1,15 +1,61 @@
-import {pipeline} from 'node:stream/promises';
-import fs from 'node:fs';
-import zlib from 'node:zlib';
+import http from "http";
+import fs from "node:fs";
+
+const server = http.createServer((req, res)=>{
+    if(req.url != '/') res.end();
+    
+    // download big file (Bad way)
+    // const file = fs.readFileSync("sample.txt");
+    // res.end(file);
+
+    // Downloading big file using good way (Streams)
+    // const readableStream = fs.createReadStream("sample.txt");
+    // readableStream -> writableStream (Pipe)
+    // NOTE - req is a readablestream and res is a writable stream.
+    // readableStream.pipe(res);
+
+    // Copy big file using bad way
+    // const file = fs.readFileSync('sample.txt');
+    // fs.writeFileSync('output.txt', file);
+    // res.end();
+
+    // copy big file good way
+    const readStream = fs.createReadStream('sample.txt');
+    const writeStream = fs.createWriteStream('output.txt');
+
+    readStream.on('data',(chunk)=>{
+      console.log(chunk)
+      writeStream.write(chunk)
+    });
+
+    res.end()
+});
+
+const PORT = 3000;
+server.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
 
 
-async function run(){
-    await pipeline(
-        fs.createReadStream('demo.txt'),
-        zlib.createGzip(),
-        fs.createWriteStream('demo.tar.gz')
-    );
-    console.log('Pipeline Succeeded');
-}
 
-run().catch(console.error);
+
+
+
+
+
+// import {pipeline} from 'node:stream/promises';
+// import fs from 'node:fs';
+// import zlib from 'node:zlib';
+
+
+// async function run(){
+//     await pipeline(
+//         fs.createReadStream('demo.txt'),
+//         zlib.createGzip(),
+//         fs.createWriteStream('demo.tar.gz')
+//     );
+//     console.log('Pipeline Succeeded');
+// }
+
+// run().catch(console.error);
+
+
+
--- streams/stream.md
+++ streams/stream.md
@@ -44,3 +44,43 @@ async function run(){
 }
 
 run().catch(console.error);
+```
+
+## Example how can we work with stream in file system.
+
+```js
+import http from "http";
+import fs from "node:fs";
+
+const server = http.createServer((req, res)=>{
+    if(req.url != '/') res.end();
+    
+    // download big file (Bad way)
+    // const file = fs.readFileSync("sample.txt");
+    // res.end(file);
+
+    // Downloading big file using good way (Streams)
+    // const readableStream = fs.createReadStream("sample.txt");
+    // readableStream -> writableStream (Pipe)
+    // NOTE - req is a readablestream and res is a writable stream.
+    // readableStream.pipe(res);
+
+    // Copy big file using bad way
+    // const file = fs.readFileSync('sample.txt');
+    // fs.writeFileSync('output.txt', file);
+    // res.end();
+
+    // copy big file good way
+    const readStream = fs.createReadStream('sample.txt');
+    const writeStream = fs.createWriteStream('output.txt');
+
+    readStream.on('data',(chunk)=>{
+      console.log(chunk)
+      writeStream.write(chunk)
+    });
+
+    res.end()
+});
+
+const PORT = 3000;
+server.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
